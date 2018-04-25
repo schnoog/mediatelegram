b3602e8dabe2777cb10b8c0a1a1ccc0b79d7ae9c
schnoog <braun.volker@gmail.com>
2018-04-25 17:33:19 +0200

    Facebook

---
+++ Commands/FacebookCommand.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * This file is part of the TelegramBot package.
+ *
+ * (c) Avtandil Kikabidze aka LONGMAN <akalongman@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Longman\TelegramBot\Commands\UserCommands;
+
+use Longman\TelegramBot\Commands\UserCommand;
+use Longman\TelegramBot\Request;
+
+/**
+ * User "/echo" command
+ *
+ * Simply echo the input back to the user.
+ */
+class FacebookCommand extends UserCommand
+{
+    /**
+     * @var string
+     */
+    protected $name = 'facebook';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Download Facebook Video';
+
+    /**
+     * @var string
+     */
+    protected $usage = '/facebook <Facebook Video Url>';
+
+    /**
+     * @var string
+     */
+    protected $version = '1.1.0';
+
+    /**
+     * Command execute method
+     *
+     * @return \Longman\TelegramBot\Entities\ServerResponse
+     * @throws \Longman\TelegramBot\Exception\TelegramException
+     */
+    public function execute()
+    {
+        $message = $this->getMessage();
+        $chat_id = $message->getChat()->getId();
+        $text    = trim($message->getText(true));
+
+        if ($text === '') {
+            $text = 'Command usage: ' . $this->getUsage();
+        }
+
+        $sendfile = GetTwitterVideo($text);
+        if(!$sendfile){
+            $data = [
+                'chat_id' => $chat_id,
+                'text'    => "No video file available, sorry",
+            ];
+            return Request::sendMessage($data);
+        }
+
+            $singleSend =   Request::sendDocument([
+                                    'caption'  => "Your file is ready",
+                                    'chat_id'  => $chat_id,
+                                    'document' => Request::encodeFile($sendfile),
+                            ]);
+            if(DELETE_AFTER) unlink($sendfile); 
+            return $singleSend;
+    }
+}
